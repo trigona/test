3d6cfecfe8ff4ec230a64fe5ed221cc9ac16c98e
Lope Soriao <buboy2k6@gmail.com>
2021-07-26 22:48:15 +0900

    my first version of hungry code

---
+++ readme.py
@@ -0,0 +1,10 @@
+# this is my readme file for test
+# it will be the repository of basic github bash  command
+# basic command add commit and push
+# youtube channel codebasics
+
+# I created git repository sub directory test
+# clone the repository on my desktop with the command
+# git clone <link>
+git clone https://github.com/trigona/test.git
+
